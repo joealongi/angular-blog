dd4aa00d58e73e6bc75f273f86470091b8fb9d10
Joe Alongi <email@heyitsjoealongi.com>
2025-07-04 16:18:21 -0400

    Prettier, ESLint, and components

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,10 @@
       <div
         class="heyitsjoealongi-theme-column flex flex-col flex-auto justify-self-center"
       >
-        <Logo />
+        <Logo
+          class="heyitsjoealongi-theme-logo w-9 h-9 md:w-18 md:h-18 lg:w-30 lg:h-30 my-3"
+          :font-controlled="false"
+        />
         <h1
           class="heyitsjoealongi-theme-heading-one my-3 text-3xl md:text-6xl subpixel-antialiased font-light text-left"
         >
@@ -28,7 +31,72 @@
           with modern code and intelligent systems—ready for today, built for
           tomorrow.
         </p>
-        <Footer />
+        <footer>
+          <ul
+            class="heyitsjoealongi-theme-footer flex flex-col md:flex-row flex-auto my-3"
+          >
+            <li class="flex items-center">
+              <a
+                class="mb-3 md:mb-0 md:mr-6 text-base antialiased font-black text-left transition-all"
+                rel="noopener noreferrer"
+                target="_blank"
+                href="https://blog.heyitsjoealongi.com/"
+              >
+                blog
+              </a>
+            </li>
+            <li class="flex items-center">
+              <a
+                class="mb-3 md:mb-0 md:mr-6 text-base antialiased font-black text-left transition-all"
+                rel="noopener noreferrer"
+                target="_blank"
+                href="https://x.com/heyitsjoealongi/"
+              >
+                x
+              </a>
+            </li>
+            <li class="flex items-center">
+              <a
+                class="mb-3 md:mb-0 md:mr-6 text-base antialiased font-black text-left transition-all"
+                rel="noopener noreferrer"
+                target="_blank"
+                href="https://github.com/heyitsjoealongi/"
+              >
+                github
+              </a>
+            </li>
+            <li class="flex items-center">
+              <a
+                class="mb-3 md:mb-0 md:mr-6 text-base antialiased font-black text-left transition-all"
+                rel="noopener noreferrer"
+                target="_blank"
+                href="https://www.instagram.com/heyitsjoealongi/"
+              >
+                instagram
+              </a>
+            </li>
+            <li class="flex items-center">
+              <a
+                class="mb-3 md:mb-0 md:mr-6 text-base antialiased font-black text-left transition-all"
+                rel="noopener noreferrer"
+                target="_blank"
+                href="https://www.linkedin.com/in/heyitsjoealongi/"
+              >
+                linkedin
+              </a>
+            </li>
+            <li class="flex items-center">
+              <a
+                class="mb-3 md:mb-0 md:mr-6 text-base antialiased font-black text-left transition-all"
+                rel="noopener noreferrer"
+                target="_blank"
+                href="https://www.youtube.com/@heyitsjoealongi/"
+              >
+                youtube
+              </a>
+            </li>
+          </ul>
+        </footer>
         <p
           className="heyitsjoealongi-theme-copyright my-3 text-xs subpixel-antialiased font-light text-left"
         >
@@ -40,6 +108,7 @@
 </template>
 
 <script lang="ts" setup>
+import Logo from "~/assets/heyitsjoealongi.svg";
 const year = new Date().getFullYear();
 </script>
 
